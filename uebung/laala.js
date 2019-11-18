4d3f6ae34025abfcfceee1139436ead539336f73
JuleHeinzmann <56443328+JuleHeinzmann@users.noreply.github.com>
2019-11-18 12:05:33 +0100

    another commit

---
+++ uebung/laala.js
@@ -0,0 +1,8 @@
+var test = "Hallo";
+var welt = "Welt";
+var zahl = 200;
+var zahl2 = 20;
+var berechnen = zahl2 / zahl * 100;
+//var prozent:string = "%";
+console.log(berechnen + "%");
+//# sourceMappingURL=laala.js.map
---
+++ uebung/laala.ts
@@ -0,0 +1,9 @@
+var test:string = "Hallo";
+
+var welt:string = "Welt";
+var zahl:number = 200;
+var zahl2:number = 20;
+var berechnen:number = zahl2/zahl*100;
+//var prozent:string = "%";
+
+console.log(berechnen+"%");
